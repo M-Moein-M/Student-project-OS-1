6acf13d78382a22752a08239e31e8ab1c623dd68
M-Moein-M <mohammadmoein.moradi@gmail.com>
2020-11-30 19:52:59 +0330

    chore: move extracting file to separated function

--- main_process.c
+++ main_process.c
@@ -2,7 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BUFFER_SIZE 50
+
 int get_process_exe_time(char* command);
+void extract_file_data(char commands[][BUFFER_SIZE], int commands_execution_time [], FILE *commands_file);
 
 int main()
 {
@@ -23,31 +26,36 @@ int main()
   // data structures for number of processes and commands and their execution time
   int n_commands = n_fst_level_process * n_scnd_level_process;
   
-  char buffer[100];
-  char commands[n_commands][100];
+  char commands[n_commands][BUFFER_SIZE];
   int commands_execution_time[n_commands];
 
+  extract_file_data(commands, commands_execution_time, commands_file);
+
+  fclose(commands_file);
+  return 0;
+}
+
+// read from file and extract all the commands and their exe time
+void extract_file_data(char commands[][BUFFER_SIZE], int commands_execution_time [], FILE *commands_file){
+  char buffer[BUFFER_SIZE];
   // reading file line by line
   int counter = 0;
-  while (fgets(buffer, 100, commands_file) != NULL){
+  while (fgets(buffer, BUFFER_SIZE, commands_file) != NULL){
         
     int len = strlen(buffer);
     buffer[len-1] = 0;
 
     int exe_time = get_process_exe_time(buffer);
-    if (exe_time == -1) continue;
-      
-    printf("%s  => ", buffer);
-    printf("%d\n", exe_time);
+    if (exe_time == -1) continue; // skip incase it was \n at the end of lines
 
     // store the results
     strcpy(commands[counter], buffer);
     commands_execution_time[counter] = exe_time;
     
+    counter++;
+    
   }
-
-  fclose(commands_file);
-  return 0;
+  return;
 }
 
 // returns last written number in each line of commands file and removes it from input string
