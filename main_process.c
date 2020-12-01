57f51cf057127272d96b54c20f515b0c4737708e
M-Moein-M <mohammadmoein.moradi@gmail.com>
2020-12-01 17:31:00 +0330

    feat: add creating 1st level processes

--- main_process.c
+++ main_process.c
@@ -1,11 +1,23 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #define BUFFER_SIZE 100
+#define READ_END 0
+#define WRITE_END 1
+
+struct Process {
+  pid_t id;
+  int pipe[2];
+  int process_level;
+};
 
 int get_process_exe_time(char* command);
 void extract_file_data(char commands[][BUFFER_SIZE], int commands_execution_time [], FILE *commands_file);
+void run_first_level_process(int pipe[2], int n_scnd_level_process);
 
 int main()
 {
@@ -31,9 +43,47 @@ int main()
 
   extract_file_data(commands, commands_execution_time, commands_file);
 
+  struct Process fst_level_processes[n_fst_level_process];
+
+  // create first level child processes
+  for (int i = 0; i < n_fst_level_process; i++){
+
+    // create new pipe for parent and new level 1 process
+    if (pipe(fst_level_processes[i].pipe) == -1){
+      printf("Pipe failed");
+      return 0;
+    }
+    
+    pid_t new_id = fork(); // create new child process
+
+    if (new_id > 0){ // parent process
+      
+      fst_level_processes[i].id = new_id;
+      fst_level_processes[i].process_level = 1; // set the process level to 1
+
+    }else if (new_id == 0){ // first level child process
+      run_first_level_process(fst_level_processes[i].pipe, n_scnd_level_process);
+      break;  // stop continuing the loop in child process
+    }
+
+  }
+  // waiting for all of the child processes
+  int stat;
+  pid_t finished_child_id;
+  while ((finished_child_id = wait(&stat)) != -1){
+    printf("child process is finished with id %d\n", finished_child_id);
+  }
+
   fclose(commands_file);
   return 0;
 }
+
+// whatever the level 1 process need to do
+void run_first_level_process(int pipe[2], int n_scnd_level_process){
+  printf("1st level Child process with id %d      pipe read/out %d/%d\n", getpid(), pipe[READ_END], pipe[WRITE_END]);
+  return;
+}
+
 
 // read from file and extract all the commands and their exe time
 void extract_file_data(char commands[][BUFFER_SIZE], int commands_execution_time [], FILE *commands_file){
